a5735c9f27164c82e475e4c24aa03def4d37485b
Tom Carden <tom@tom-carden.co.uk>
2009-11-21 15:40:47 -0800

    first commit

---
+++ binary.js
@@ -0,0 +1,363 @@
+//+ Jonas Raoni Soares Silva
+//@ http://jsfromhell.com/classes/binary-parser [rev. #1]
+
+BinaryParser = function(bigEndian, allowExceptions){
+    this.bigEndian = bigEndian, this.allowExceptions = allowExceptions;
+};
+with({p: BinaryParser.prototype}){
+    p.encodeFloat = function(number, precisionBits, exponentBits){
+        var bias = Math.pow(2, exponentBits - 1) - 1, minExp = -bias + 1, maxExp = bias, minUnnormExp = minExp - precisionBits,
+        status = isNaN(n = parseFloat(number)) || n == -Infinity || n == +Infinity ? n : 0,
+        exp = 0, len = 2 * bias + 1 + precisionBits + 3, bin = new Array(len),
+        signal = (n = status !== 0 ? 0 : n) < 0, n = Math.abs(n), intPart = Math.floor(n), floatPart = n - intPart,
+        i, lastBit, rounded, j, result;
+        for(i = len; i; bin[--i] = 0);
+        for(i = bias + 2; intPart && i; bin[--i] = intPart % 2, intPart = Math.floor(intPart / 2));
+        for(i = bias + 1; floatPart > 0 && i; (bin[++i] = ((floatPart *= 2) >= 1) - 0) && --floatPart);
+        for(i = -1; ++i < len && !bin[i];);
+        if(bin[(lastBit = precisionBits - 1 + (i = (exp = bias + 1 - i) >= minExp && exp <= maxExp ? i + 1 : bias + 1 - (exp = minExp - 1))) + 1]){
+            if(!(rounded = bin[lastBit]))
+                for(j = lastBit + 2; !rounded && j < len; rounded = bin[j++]);
+            for(j = lastBit + 1; rounded && --j >= 0; (bin[j] = !bin[j] - 0) && (rounded = 0));
+        }
+        for(i = i - 2 < 0 ? -1 : i - 3; ++i < len && !bin[i];);
+
+        (exp = bias + 1 - i) >= minExp && exp <= maxExp ? ++i : exp < minExp &&
+            (exp != bias + 1 - len && exp < minUnnormExp && this.warn("encodeFloat::float underflow"), i = bias + 1 - (exp = minExp - 1));
+        (intPart || status !== 0) && (this.warn(intPart ? "encodeFloat::float overflow" : "encodeFloat::" + status),
+            exp = maxExp + 1, i = bias + 2, status == -Infinity ? signal = 1 : isNaN(status) && (bin[i] = 1));
+        for(n = Math.abs(exp + bias), j = exponentBits + 1, result = ""; --j; result = (n % 2) + result, n = n >>= 1);
+        for(n = 0, j = 0, i = (result = (signal ? "1" : "0") + result + bin.slice(i, i + precisionBits).join("")).length, r = [];
+            i; n += (1 << j) * result.charAt(--i), j == 7 && (r[r.length] = String.fromCharCode(n), n = 0), j = (j + 1) % 8);
+        r[r.length] = n ? String.fromCharCode(n) : "";
+        return (this.bigEndian ? r.reverse() : r).join("");
+    };
+    p.encodeInt = function(number, bits, signed){
+        var max = Math.pow(2, bits), r = [];
+        (number >= max || number < -(max >> 1)) && this.warn("encodeInt::overflow") && (number = 0);
+        number < 0 && (number += max);
+        for(; number; r[r.length] = String.fromCharCode(number % 256), number = Math.floor(number / 256));
+        for(bits = -(-bits >> 3) - r.length; bits--; r[r.length] = "\0");
+        return (this.bigEndian ? r.reverse() : r).join("");
+    };
+    p.decodeFloat = function(data, precisionBits, exponentBits){
+        var b = ((b = new this.Buffer(this.bigEndian, data)).checkBuffer(precisionBits + exponentBits + 1), b),
+            bias = Math.pow(2, exponentBits - 1) - 1, signal = b.readBits(precisionBits + exponentBits, 1),
+            exponent = b.readBits(precisionBits, exponentBits), significand = 0,
+            divisor = 2, curByte = b.buffer.length + (-precisionBits >> 3) - 1,
+            byteValue, startBit, mask;
+        do
+            for(byteValue = b.buffer[ ++curByte ], startBit = precisionBits % 8 || 8, mask = 1 << startBit;
+                mask >>= 1; (byteValue & mask) && (significand += 1 / divisor), divisor *= 2);
+        while(precisionBits -= startBit);
+        return exponent == (bias << 1) + 1 ? significand ? NaN : signal ? -Infinity : +Infinity
+            : (1 + signal * -2) * (exponent || significand ? !exponent ? Math.pow(2, -bias + 1) * significand
+            : Math.pow(2, exponent - bias) * (1 + significand) : 0);
+    };
+    p.decodeInt = function(data, bits, signed){
+        var b = new this.Buffer(this.bigEndian, data), x = b.readBits(0, bits), max = Math.pow(2, bits);
+        return signed && x >= max / 2 ? x - max : x;
+    };
+    with({p: (p.Buffer = function(bigEndian, buffer){
+        this.bigEndian = bigEndian || 0, this.buffer = [], this.setBuffer(buffer);
+    }).prototype}){
+        p.readBits = function(start, length){
+            //shl fix: Henri Torgemane ~1996 (compressed by Jonas Raoni)
+            function shl(a, b){
+                for(++b; --b; a = ((a %= 0x7fffffff + 1) & 0x40000000) == 0x40000000 ? a * 2 : (a - 0x40000000) * 2 + 0x7fffffff + 1);
+                return a;
+            }
+            if(start < 0 || length <= 0)
+                return 0;
+            this.checkBuffer(start + length);
+            for(var offsetLeft, offsetRight = start % 8, curByte = this.buffer.length - (start >> 3) - 1,
+                lastByte = this.buffer.length + (-(start + length) >> 3), diff = curByte - lastByte,
+                sum = ((this.buffer[ curByte ] >> offsetRight) & ((1 << (diff ? 8 - offsetRight : length)) - 1))
+                + (diff && (offsetLeft = (start + length) % 8) ? (this.buffer[ lastByte++ ] & ((1 << offsetLeft) - 1))
+                << (diff-- << 3) - offsetRight : 0); diff; sum += shl(this.buffer[ lastByte++ ], (diff-- << 3) - offsetRight)
+            );
+            return sum;
+        };
+        p.setBuffer = function(data){
+            if(data){
+                for(var l, i = l = data.length, b = this.buffer = new Array(l); i; b[l - i] = data.charCodeAt(--i));
+                this.bigEndian && b.reverse();
+            }
+        };
+        p.hasNeededBits = function(neededBits){
+            return this.buffer.length >= -(-neededBits >> 3);
+        };
+        p.checkBuffer = function(neededBits){
+            if(!this.hasNeededBits(neededBits))
+                throw new Error("checkBuffer::missing bytes");
+        };
+    }
+    p.warn = function(msg){
+        if(this.allowExceptions)
+            throw new Error(msg);
+        return 1;
+    };
+    p.toSmall = function(data){return this.decodeInt(data, 8, true);};
+    p.fromSmall = function(number){return this.encodeInt(number, 8, true);};
+    p.toByte = function(data){return this.decodeInt(data, 8, false);};
+    p.fromByte = function(number){return this.encodeInt(number, 8, false);};
+    p.toShort = function(data){return this.decodeInt(data, 16, true);};
+    p.fromShort = function(number){return this.encodeInt(number, 16, true);};
+    p.toWord = function(data){return this.decodeInt(data, 16, false);};
+    p.fromWord = function(number){return this.encodeInt(number, 16, false);};
+    p.toInt = function(data){return this.decodeInt(data, 32, true);};
+    p.fromInt = function(number){return this.encodeInt(number, 32, true);};
+    p.toDWord = function(data){return this.decodeInt(data, 32, false);};
+    p.fromDWord = function(number){return this.encodeInt(number, 32, false);};
+    p.toFloat = function(data){return this.decodeFloat(data, 23, 8);};
+    p.fromFloat = function(number){return this.encodeFloat(number, 23, 8);};
+    p.toDouble = function(data){return this.decodeFloat(data, 52, 11);};
+    p.fromDouble = function(number){return this.encodeFloat(number, 52, 11);};
+}
+
+/*
+ * Binary Ajax 0.1.7
+ * Copyright (c) 2008 Jacob Seidelin, cupboy@gmail.com, http://blog.nihilogic.dk/
+ * Licensed under the MPL License [http://www.nihilogic.dk/licenses/mpl-license.txt]
+ */
+
+
+var BinaryFile = function(strData, iDataOffset, iDataLength) {
+	var data = strData;
+	var dataOffset = iDataOffset || 0;
+	var dataLength = 0;
+
+	this.getRawData = function() {
+		return data;
+	}
+
+	if (typeof strData == "string") {
+		dataLength = iDataLength || data.length;
+
+		this.getByteAt = function(iOffset) {
+			return data.charCodeAt(iOffset + dataOffset) & 0xFF;
+		}
+	} else if (typeof strData == "unknown") {
+		dataLength = iDataLength || IEBinary_getLength(data);
+
+		this.getByteAt = function(iOffset) {
+			return IEBinary_getByteAt(data, iOffset + dataOffset);
+		}
+	}
+
+	this.getLength = function() {
+		return dataLength;
+	}
+
+	this.getSByteAt = function(iOffset) {
+		var iByte = this.getByteAt(iOffset);
+		if (iByte > 127)
+			return iByte - 256;
+		else
+			return iByte;
+	}
+
+	this.getShortAt = function(iOffset, bBigEndian) {
+		var iShort = bBigEndian ? 
+			(this.getByteAt(iOffset) << 8) + this.getByteAt(iOffset + 1)
+			: (this.getByteAt(iOffset + 1) << 8) + this.getByteAt(iOffset)
+		if (iShort < 0) iShort += 65536;
+		return iShort;
+	}
+	this.getSShortAt = function(iOffset, bBigEndian) {
+		var iUShort = this.getShortAt(iOffset, bBigEndian);
+		if (iUShort > 32767)
+			return iUShort - 65536;
+		else
+			return iUShort;
+	}
+	this.getLongAt = function(iOffset, bBigEndian) {
+		var iByte1 = this.getByteAt(iOffset),
+			iByte2 = this.getByteAt(iOffset + 1),
+			iByte3 = this.getByteAt(iOffset + 2),
+			iByte4 = this.getByteAt(iOffset + 3);
+
+		var iLong = bBigEndian ? 
+			(((((iByte1 << 8) + iByte2) << 8) + iByte3) << 8) + iByte4
+			: (((((iByte4 << 8) + iByte3) << 8) + iByte2) << 8) + iByte1;
+		if (iLong < 0) iLong += 4294967296;
+		return iLong;
+	}
+	this.getSLongAt = function(iOffset, bBigEndian) {
+		var iULong = this.getLongAt(iOffset, bBigEndian);
+		if (iULong > 2147483647)
+			return iULong - 4294967296;
+		else
+			return iULong;
+	}
+	this.getStringAt = function(iOffset, iLength) {
+		var aStr = [];
+		for (var i=iOffset,j=0;i<iOffset+iLength;i++,j++) {
+			aStr[j] = String.fromCharCode(this.getByteAt(i));
+		}
+		return aStr.join("");
+	}
+
+        this.parser = new BinaryParser();
+
+	this.getDoubleAt = function(iOffset, bBigEndian) {
+		var data = this.getStringAt(iOffset,8);
+                this.parser.bigEndian = bBigEndian;
+                return this.parser.toDouble(data);
+	}
+
+	this.getCharAt = function(iOffset) {
+		return String.fromCharCode(this.getByteAt(iOffset));
+	}
+	this.toBase64 = function() {
+		return window.btoa(data);
+	}
+	this.fromBase64 = function(strBase64) {
+		data = window.atob(strBase64);
+	}
+}
+
+
+var BinaryAjax = (function() {
+
+	function createRequest() {
+		var oHTTP = null;
+		if (window.XMLHttpRequest) {
+			oHTTP = new XMLHttpRequest();
+		} else if (window.ActiveXObject) {
+			oHTTP = new ActiveXObject("Microsoft.XMLHTTP");
+		}
+		return oHTTP;
+	}
+
+	function getHead(strURL, fncCallback, fncError) {
+		var oHTTP = createRequest();
+		if (oHTTP) {
+			if (fncCallback) {
+				if (typeof(oHTTP.onload) != "undefined") {
+					oHTTP.onload = function() {
+						if (oHTTP.status == "200") {
+							fncCallback(this);
+						} else {
+							if (fncError) fncError();
+						}
+						oHTTP = null;
+					};
+				} else {
+					oHTTP.onreadystatechange = function() {
+						if (oHTTP.readyState == 4) {
+							if (oHTTP.status == "200") {
+								fncCallback(this);
+							} else {
+								if (fncError) fncError();
+							}
+							oHTTP = null;
+						}
+					};
+				}
+			}
+			oHTTP.open("HEAD", strURL, true);
+			oHTTP.send(null);
+		} else {
+			if (fncError) fncError();
+		}
+	}
+
+	function sendRequest(strURL, fncCallback, fncError, aRange, bAcceptRanges, iFileSize) {
+		var oHTTP = createRequest();
+		if (oHTTP) {
+
+			var iDataOffset = 0;
+			if (aRange && !bAcceptRanges) {
+				iDataOffset = aRange[0];
+			}
+			var iDataLen = 0;
+			if (aRange) {
+				iDataLen = aRange[1]-aRange[0]+1;
+			}
+
+			if (fncCallback) {
+				if (typeof(oHTTP.onload) != "undefined") {
+					oHTTP.onload = function() {
+
+						if (oHTTP.status == "200" || oHTTP.status == "206" || oHTTP.status == "0") {
+							oHTTP.binaryResponse = new BinaryFile(oHTTP.responseText, iDataOffset, iDataLen);
+							oHTTP.fileSize = iFileSize || oHTTP.getResponseHeader("Content-Length");
+							fncCallback(oHTTP);
+						} else {
+							if (fncError) fncError();
+						}
+						oHTTP = null;
+					};
+				} else {
+					oHTTP.onreadystatechange = function() {
+						if (oHTTP.readyState == 4) {
+							if (oHTTP.status == "200" || oHTTP.status == "206" || oHTTP.status == "0") {
+								// IE6 craps if we try to extend the XHR object
+								var oRes = {
+									status : oHTTP.status,
+									binaryResponse : new BinaryFile(oHTTP.responseBody, iDataOffset, iDataLen),
+									fileSize : iFileSize || oHTTP.getResponseHeader("Content-Length")
+								};
+								fncCallback(oRes);
+							} else {
+								if (fncError) fncError();
+							}
+							oHTTP = null;
+						}
+					};
+				}
+			}
+			oHTTP.open("GET", strURL, true);
+
+			if (oHTTP.overrideMimeType) oHTTP.overrideMimeType('text/plain; charset=x-user-defined');
+
+			if (aRange && bAcceptRanges) {
+				oHTTP.setRequestHeader("Range", "bytes=" + aRange[0] + "-" + aRange[1]);
+			}
+
+			oHTTP.setRequestHeader("If-Modified-Since", "Sat, 1 Jan 1970 00:00:00 GMT");
+
+			oHTTP.send(null);
+		} else {
+			if (fncError) fncError();
+		}
+	}
+
+	return function(strURL, fncCallback, fncError, aRange) {
+
+		if (aRange) {
+			getHead(
+				strURL, 
+				function(oHTTP) {
+					var iLength = parseInt(oHTTP.getResponseHeader("Content-Length"),10);
+					var strAcceptRanges = oHTTP.getResponseHeader("Accept-Ranges");
+
+					var iStart, iEnd;
+					iStart = aRange[0];
+					if (aRange[0] < 0) 
+						iStart += iLength;
+					iEnd = iStart + aRange[1] - 1;
+
+					sendRequest(strURL, fncCallback, fncError, [iStart, iEnd], (strAcceptRanges == "bytes"), iLength);
+				}
+			);
+
+		} else {
+			sendRequest(strURL, fncCallback, fncError);
+		}
+	}
+
+}());
+
+
+document.write(
+	"<script type='text/vbscript'>\r\n"
+	+ "Function IEBinary_getByteAt(strBinary, iOffset)\r\n"
+	+ "	IEBinary_getByteAt = AscB(MidB(strBinary,iOffset+1,1))\r\n"
+	+ "End Function\r\n"
+	+ "Function IEBinary_getLength(strBinary)\r\n"
+	+ "	IEBinary_getLength = LenB(strBinary)\r\n"
+	+ "End Function\r\n"
+	+ "</script>\r\n"
+);
---
+++ shapefile.js
@@ -0,0 +1,346 @@
+
+/**
+ * The ShpType class is a place holder for the ESRI Shapefile defined
+ * shape types.
+ * @author Edwin van Rijkom
+ * 
+ */     
+var ShpType = {
+
+    /**
+     * Unknow Shape Type (for internal use) 
+     */
+    SHAPE_UNKNOWN : -1,
+    /**
+     * ESRI Shapefile Null Shape shape type.
+     */     
+    SHAPE_NULL : 0,
+    /**
+     * ESRI Shapefile Point Shape shape type.
+     */
+    SHAPE_POINT : 1,
+    /**
+     * ESRI Shapefile PolyLine Shape shape type.
+     */
+    SHAPE_POLYLINE : 3,
+    /**
+     * ESRI Shapefile Polygon Shape shape type.
+     */
+    SHAPE_POLYGON : 5,
+    /**
+     * ESRI Shapefile Multipoint Shape shape type
+     * (currently unsupported).
+     */
+    SHAPE_MULTIPOINT : 8,
+    /**
+     * ESRI Shapefile PointZ Shape shape type.
+     */
+    SHAPE_POINTZ : 11,
+    /**
+     * ESRI Shapefile PolylineZ Shape shape type
+     * (currently unsupported).
+     */
+    SHAPE_POLYLINEZ : 13,
+    /**
+     * ESRI Shapefile PolygonZ Shape shape type
+     * (currently unsupported).
+     */
+    SHAPE_POLYGONZ : 15,
+    /**
+     * ESRI Shapefile MultipointZ Shape shape type
+     * (currently unsupported).
+     */
+    SHAPE_MULTIPOINTZ : 18,
+    /**
+     * ESRI Shapefile PointM Shape shape type
+     */
+    SHAPE_POINTM : 21,
+    /**
+     * ESRI Shapefile PolyLineM Shape shape type
+     * (currently unsupported).
+     */
+    SHAPE_POLYLINEM : 23,
+    /**
+     * ESRI Shapefile PolygonM Shape shape type
+     * (currently unsupported).
+     */
+    SHAPE_POLYGONM : 25,
+    /**
+     * ESRI Shapefile MultiPointM Shape shape type
+     * (currently unsupported).
+     */
+    SHAPE_MULTIPOINTM : 28,
+    /**
+     * ESRI Shapefile MultiPatch Shape shape type
+     * (currently unsupported).
+     */
+    SHAPE_MULTIPATCH : 31
+
+};
+
+function Point(x,y) {
+  this.x = x;
+  this.y = y;
+}
+
+function Rectangle(x,y,w,h) {
+  this.x = x;
+  this.y = y;
+  this.w = w;
+  this.h = h;
+}
+
+/**
+ * Constructor.
+ * @param src
+ * @return
+ * @throws ShpError Not a valid shape file header
+ * @throws ShpError Not a valid signature
+ * 
+ */                     
+function ShapeHeader(src, binState)
+{
+    if (src.getLength() < 100)
+        alert("Not a valid shape file header (too small)");
+
+    if (src.getSLongAt(binState.offset, binState.bigEndian) != 9994)
+        alert("Not a valid signature. Expected 9994");
+    binState.offset += 4; 
+ 
+    // skip 5 integers;
+    binState.offset += 5*4;
+    
+    // read file-length:
+    this.fileLength = src.getSLongAt(binState.offset, binState.bigEndian);
+    binState.offset += 4;
+ 
+    // switch endian:
+    binState.bigEndian = false;
+    
+    // read version:
+    this.version = src.getSLongAt(binState.offset, binState.bigEndian);
+    binState.offset += 4;
+
+    // read shape-type:
+    this.shapeType = src.getSLongAt(binState.offset, binState.bigEndian);
+    binState.offset += 4;
+   
+    var x = src.getDoubleAt(binState.offset, binState.bigEndian);
+    binState.offset += 8;
+    var y = src.getDoubleAt(binState.offset, binState.bigEndian);
+    binState.offset += 8;
+    var w = src.getDoubleAt(binState.offset, binState.bigEndian);
+    binState.offset += 8;
+    var h = src.getDoubleAt(binState.offset, binState.bigEndian);
+    binState.offset += 8;
+ 
+    // read bounds:
+    this.boundsXY = new Rectangle(x,y,w,h);
+    
+    x = src.getDoubleAt(binState.offset, binState.bigEndian);
+    binState.offset += 8;
+    y = src.getDoubleAt(binState.offset, binState.bigEndian);
+    binState.offset += 8;
+    
+    this.boundsZ = new Point(x,y);
+    
+    x = src.getDoubleAt(binState.offset, binState.bigEndian);
+    binState.offset += 8;
+    y = src.getDoubleAt(binState.offset, binState.bigEndian);
+    binState.offset += 8;
+
+    this.boundsM = new Point(x,y);
+}
+
+
+function ShpRecord(src, binState) {
+    var availableBytes = src.getLength() - binState.offset;
+    
+    if (availableBytes == 0) 
+            throw(new Error("No Data"));
+            
+    if (availableBytes < 8)
+            throw(new Error("Not a valid record header (too small)"));
+    
+    binState.bigEndian = true;
+    
+    this.number = src.getSLongAt(binState.offset, binState.bigEndian);
+    binState.offset += 4;
+    this.contentLength = src.getSLongAt(binState.offset, binState.bigEndian);
+    binState.offset += 4;
+    this.contentLengthBytes = this.contentLength*2 - 4;                      
+    binState.bigEndian = false;
+    var shapeOffset = binState.offset;
+    this.shapeType = src.getSLongAt(binState.offset, binState.bigEndian);
+    binState.offset += 4;
+                    
+    switch(this.shapeType) {
+            case ShpType.SHAPE_POINT:
+                    this.shape = new ShpPoint(src, this.contentLengthBytes, binState);
+                    break;
+            case ShpType.SHAPE_POINTZ:
+                    this.shape = new ShpPointZ(src, this.contentLengthBytes, binState);
+                    break;
+            case ShpType.SHAPE_POLYGON:
+                    this.shape = new ShpPolygon(src, this.contentLengthBytes, binState);
+                    break;
+            case ShpType.SHAPE_POLYLINE:
+                    this.shape = new ShpPolyline(src, this.contentLengthBytes, binState);
+                    break;
+            case ShpType.SHAPE_MULTIPATCH:
+            case ShpType.SHAPE_MULTIPOINT:
+            case ShpType.SHAPE_MULTIPOINTM:
+            case ShpType.SHAPE_MULTIPOINTZ:
+            case ShpType.SHAPE_POINTM:
+            case ShpType.SHAPE_POLYGONM:
+            case ShpType.SHAPE_POLYGONZ:
+            case ShpType.SHAPE_POLYLINEZ:
+            case ShpType.SHAPE_POLYLINEM:
+                    throw(new Error(this.shapeType+" Shape type is currently unsupported by this library"));
+                    break;  
+            default:        
+                    throw(new Error("Encountered unknown shape type ("+this.shapeType+")"));
+                    break;
+    }
+}
+
+function ShpPoint(src, size, binState) {
+    this.type = ShpType.SHAPE_POINT;
+    if (src) {                      
+        if (src.getLength() - binState.offset < size)
+            throw(new Error("Not a Point record (to small)"));
+        this.x = (size > 0)  ? src.getDoubleAt(binState.offset, binState.bigEndian) : NaN;
+        binState.offset += 8;
+        this.y = (size > 0)  ? src.getDoubleAt(binState.offset, binState.bigEndian) : NaN;
+        binState.offset += 8;
+    }
+}
+function ShpPointZ(src, size, binState) {
+    this.type = ShpType.SHAPE_POINTZ;
+    if (src) {
+        if (src.getLength() - binState.offset < size)
+            throw(new Error("Not a Point record (to small)"));
+        this.x = (size > 0)  ? src.getDoubleAt(binState.offset, binState.bigEndian) : NaN;
+        binState.offset += 8;
+        this.y = (size > 0)  ? src.getDoubleAt(binState.offset, binState.bigEndian) : NaN;
+        binState.offset += 8;
+	this.z = (size > 16) ? src.getDoubleAt(binState.offset, binState.bigEndian) : NaN;                       
+        binState.offset += 8;
+	this.m = (size > 24) ? src.getDoubleAt(binState.offset, binState.bigEndian) : NaN;
+        binState.offset += 8;
+    }
+}
+function ShpPolygon(src, size, binState) {
+    this.type = ShpType.SHAPE_POLYGON;
+    this.rings = [];             
+    if (src) {                      
+            if (src.getLength() - binState.offset < size)
+                    throw(new Error("Not a Polygon record (to small)"));
+            
+            binState.bigEndian = false;
+            
+            var x = src.getDoubleAt(binState.offset, binState.bigEndian);
+            binState.offset += 8;
+            var y = src.getDoubleAt(binState.offset, binState.bigEndian);
+            binState.offset += 8;
+            var w = src.getDoubleAt(binState.offset, binState.bigEndian);
+            binState.offset += 8;
+            var h = src.getDoubleAt(binState.offset, binState.bigEndian);
+            binState.offset += 8;
+            
+            this.box = new Rectangle(x,y,w,h);
+                    
+            var rc = src.getSLongAt(binState.offset, binState.bigEndian);
+            binState.offset += 4;
+            var pc = src.getSLongAt(binState.offset, binState.bigEndian);
+            binState.offset += 4;
+            
+            var ringOffsets = [];
+            while(rc--) {
+                var ringOffset = src.getSLongAt(binState.offset, binState.bigEndian);
+                binState.offset += 4;
+                ringOffsets.push(ringOffset);
+            }
+            
+            var points = [];                 
+            while(pc--) {
+                points.push(new ShpPoint(src,16,binState));
+            }
+            
+            // convert points, and ringOffsets arrays to an array of rings:
+            var removed = 0;
+            var split;
+            ringOffsets.shift();                    
+            while(ringOffsets.length) {
+                    split = ringOffsets.shift();
+                    this.rings.push(points.splice(0,split-removed));
+                    removed = split;
+            }       
+            this.rings.push(points);                                     
+    }
+}
+function ShpPolyline(src, size, binState) {
+    this.type = ShpType.SHAPE_POLYLINE;
+    this.rings = [];             
+    if (src) {                      
+            if (src.getLength() - binState.offset < size)
+                    throw(new Error("Not a Polygon record (to small)"));
+            
+            binState.bigEndian = false;
+            
+            var x = src.getDoubleAt(binState.offset, binState.bigEndian);
+            binState.offset += 8;
+            var y = src.getDoubleAt(binState.offset, binState.bigEndian);
+            binState.offset += 8;
+            var w = src.getDoubleAt(binState.offset, binState.bigEndian);
+            binState.offset += 8;
+            var h = src.getDoubleAt(binState.offset, binState.bigEndian);
+            binState.offset += 8;
+            
+            this.box = new Rectangle(x,y,w,h);
+                    
+            var rc = src.getSLongAt(binState.offset, binState.bigEndian);
+            binState.offset += 4;
+            var pc = src.getSLongAt(binState.offset, binState.bigEndian);
+            binState.offset += 4;
+            
+            var ringOffsets = [];
+            while(rc--) {
+                var ringOffset = src.getSLongAt(binState.offset, binState.bigEndian);
+                binState.offset += 4;
+                ringOffsets.push(ringOffset);
+            }
+            
+            var points = [];                 
+            while(pc--) {
+                points.push(new ShpPoint(src,16,binState));
+            }
+            
+            // convert points, and ringOffsets arrays to an array of rings:
+            var removed = 0;
+            var split;
+            ringOffsets.shift();                    
+            while(ringOffsets.length) {
+                    split = ringOffsets.shift();
+                    this.rings.push(points.splice(0,split-removed));
+                    removed = split;
+            }       
+            this.rings.push(points);                                     
+    }
+}
+
+var ShpTools = {
+    readRecords: function(src, binState) {
+        var records = [];
+        while (true) {                  
+            try {           
+                records.push(new ShpRecord(src, binState));
+            }
+            catch (e) {
+                alert(e);
+                break;
+            }
+        }
+        return records;
+    }
+};
