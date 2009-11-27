eaab62951446317e44eab1d8061ccdd64ad123b5
RandomEtc <tom@tom-carden.co.uk>
2009-11-26 19:53:13 -0700

    more formatting fixes

--- dbf.js
+++ dbf.js
@@ -2,121 +2,121 @@
 
 function DbfFile(binFile) {
 
-  this.src = new BinaryFileWrapper(binFile);
+    this.src = new BinaryFileWrapper(binFile);
 
-  var t1 = new Date().getTime();  
+    var t1 = new Date().getTime();    
 
-  this.header = new DbfHeader(this.src);
+    this.header = new DbfHeader(this.src);
 
-  var t2 = new Date().getTime();
-  if (window.console && window.console.log) console.log('parsed dbf header in ' + (t2-t1) + ' ms');  
+    var t2 = new Date().getTime();
+    if (window.console && window.console.log) console.log('parsed dbf header in ' + (t2-t1) + ' ms');    
 
-  t1 = new Date().getTime();  
-  
-  // TODO: could maybe be smarter about this and only parse these on demand
-  this.records = [];
-  for (var i = 0; i < this.header.recordCount; i++) {
-    var record = this.getRecord(i);
-    this.records.push(record);
-  }  
+    t1 = new Date().getTime();    
+    
+    // TODO: could maybe be smarter about this and only parse these on demand
+    this.records = [];
+    for (var i = 0; i < this.header.recordCount; i++) {
+        var record = this.getRecord(i);
+        this.records.push(record);
+    }    
 
-  t2 = new Date().getTime();
-  if (window.console && window.console.log) console.log('parsed dbf records in ' + (t2-t1) + ' ms');  
-  
+    t2 = new Date().getTime();
+    if (window.console && window.console.log) console.log('parsed dbf records in ' + (t2-t1) + ' ms');    
+    
 }
 DbfFile.prototype.getRecord = function(index) {
-  if (index > this.header.recordCount) 
-    throw(new DbfError("",DbfError.ERROR_OUTOFBOUNDS));
 
-  this.src.position = this.header.recordsOffset + index * this.header.recordSize;
-  this.src.bigEndian = false;
+    if (index > this.header.recordCount) {
+        throw(new DbfError("",DbfError.ERROR_OUTOFBOUNDS));
+    }
 
-  return new DbfRecord(this.src, this.header);
+    this.src.position = this.header.recordsOffset + index * this.header.recordSize;
+    this.src.bigEndian = false;
+
+    return new DbfRecord(this.src, this.header);
 }
 
 
 function DbfHeader(src) {
-  
-  // endian:
-  src.bigEndian = false;
+    
+    // endian:
+    src.bigEndian = false;
 
-  this.version = src.getSByte();
-  this.updateYear = 1900+src.getByte();
-  this.updateMonth = src.getByte();
-  this.updateDay = src.getByte();
-  this.recordCount = src.getLong();
-  this.headerSize = src.getShort();
-  this.recordSize = src.getShort();
+    this.version = src.getSByte();
+    this.updateYear = 1900+src.getByte();
+    this.updateMonth = src.getByte();
+    this.updateDay = src.getByte();
+    this.recordCount = src.getLong();
+    this.headerSize = src.getShort();
+    this.recordSize = src.getShort();
 
-  //skip 2:
-  src.position += 2;
+    //skip 2:
+    src.position += 2;
 
-  this.incompleteTransaction = src.getByte();
-  this.encrypted = src.getByte();
+    this.incompleteTransaction = src.getByte();
+    this.encrypted = src.getByte();
 
-  // skip 12:
-  src.position += 12;
+    // skip 12:
+    src.position += 12;
 
-  this.mdx = src.getByte();
-  this.language = src.getByte();
+    this.mdx = src.getByte();
+    this.language = src.getByte();
 
-  // skip 2;
-  src.position += 2;
+    // skip 2;
+    src.position += 2;
 
-  // iterate field descriptors:
-  this.fields = [];
-  while (src.getSByte() != 0x0D){
-    src.position -= 1;
-    this.fields.push(new DbfField(src));
-  }
+    // iterate field descriptors:
+    this.fields = [];
+    while (src.getSByte() != 0x0D){
+        src.position -= 1;
+        this.fields.push(new DbfField(src));
+    }
 
-  this.recordsOffset = this.headerSize+1;                                  
-  
-}        
+    this.recordsOffset = this.headerSize+1;                                                                    
+    
+}                
 
 function DbfField(src) {
 
-  this.name = this.readZeroTermANSIString(src);
+    this.name = this.readZeroTermANSIString(src);
 
-  // fixed length: 10, so:
-  src.position += (10-this.name.length);
+    // fixed length: 10, so:
+    src.position += (10-this.name.length);
 
-  this.type = src.getByte();
-  this.address = src.getLong();
-  this.length = src.getByte();
-  this.decimals = src.getByte();
+    this.type = src.getByte();
+    this.address = src.getLong();
+    this.length = src.getByte();
+    this.decimals = src.getByte();
 
-  // skip 2:
-  src.position += 2;
+    // skip 2:
+    src.position += 2;
 
-  this.id = src.getByte();
+    this.id = src.getByte();
 
-  // skip 2:
-  src.position += 2;
+    // skip 2:
+    src.position += 2;
 
-  this.setFlag = src.getByte();
+    this.setFlag = src.getByte();
 
-  // skip 7:
-  src.position += 7;
+    // skip 7:
+    src.position += 7;
 
-  this.indexFlag = src.getByte();
+    this.indexFlag = src.getByte();
 }
 DbfField.prototype.readZeroTermANSIString = function(src) {
-  var r = "";
-  var b;
-  while (b = src.getByte()) {
-    r+= String.fromCharCode(b);
-  }
-  return r;
+    var r = "";
+    var b;
+    while (b = src.getByte()) {
+        r += String.fromCharCode(b);
+    }
+    return r;
 }
 
 function DbfRecord(src, header) {
-  this.offset = src.position;
-  this.values = {}
-  for (var i = 0; i < header.fields.length; i++) {
-    var field = header.fields[i];
-    this.values[field.name] = src.getString(field.length);
-  }               
+    this.offset = src.position;
+    this.values = {}
+    for (var i = 0; i < header.fields.length; i++) {
+        var field = header.fields[i];
+        this.values[field.name] = src.getString(field.length);
+    }                             
 }
-
-
