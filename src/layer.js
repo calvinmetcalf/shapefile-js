1b6d970fb3619b4fa4071c3069a9c638e8c6c567
Tom Carden <tom@stamen.com>
2009-12-07 00:12:06 -0800

    moved Map and Layer to new files, added simple label overlap check

---
+++ src/layer.js
@@ -0,0 +1,195 @@
+function Layer(url, style) {
+
+  var shpURL = url+'.shp';
+  var dbfURL = url+'.dbf';
+  this.style = style;
+
+  var theLayer = this;
+  
+  this.render = function() {
+    // it's a little bit "this"-ish... how to put all these member vars into scope?
+    if (this.dbfFile && this.shpFile) {
+      var ctx = this.canvas.getContext('2d');
+      ctx.clearRect(0,0,this.canvas.width,this.canvas.height);
+      if (this.shpFile.header.shapeType == ShpType.SHAPE_POLYGON || this.shpFile.header.shapeType == ShpType.SHAPE_POLYLINE) {
+        renderPolygons(this.canvas, this.shpFile.records, this.dbfFile.records, this.box, this.style);
+      }
+      else if (this.shpFile.header.shapeType == ShpType.SHAPE_POINT) {
+        renderPoints(this.canvas, this.shpFile.records, this.dbfFile.records, this.box, this.style);
+      }
+    }
+  }
+
+  var onShpFail = function() { 
+    alert('failed to load ' + theLayer.shpURL);
+  };
+  var onDbfFail = function() { 
+    alert('failed to load ' + theLayer.dbfURL);
+  }
+
+  var onShpComplete = function(oHTTP) {
+    var binFile = oHTTP.binaryResponse;
+    log('got data for ' + theLayer.shpURL + ', parsing shapefile');
+    theLayer.shpFile = new ShpFile(binFile);
+    if (theLayer.dbfFile) theLayer.render();
+  }
+
+  var onDbfComplete = function(oHTTP) {
+    var binFile = oHTTP.binaryResponse;
+    log('got data for ' + theLayer.dbfURL + ', parsing dbf file');
+    theLayer.dbfFile = new DbfFile(binFile);
+    if (theLayer.shpFile) theLayer.render();
+  }  
+
+  this.load = function() {
+    this.shpURL = shpURL;
+    this.dbfURL = dbfURL;
+    this.shpLoader = new BinaryAjax(shpURL, onShpComplete, onShpFail);
+    this.dbfLoader = new BinaryAjax(dbfURL, onDbfComplete, onDbfFail);
+  }
+}
+
+function renderPoints(canvas, records, data, box, style) {
+
+  log('rendering points');
+
+  var t1 = new Date().getTime();
+  log('starting rendering...');
+
+  var ctx = canvas.getContext('2d');
+  
+  var sc = Math.min(canvas.width / box.width, canvas.height / box.height);
+
+  if (style.fillStyle) ctx.fillStyle = style.fillStyle;
+  if (style.strokeStyle) ctx.strokeStyle = style.strokeStyle;
+  if (style.lineWidth) ctx.lineWidth = style.lineWidth;
+
+  // TODO: style attributes for point type (circle, square) and size/radius
+
+  for (var i = 0; i < records.length; i++) {
+    var record = records[i];
+    if (record.shapeType == ShpType.SHAPE_POINT) {
+      var shp = record.shape;
+      if (style.fillStyle) {
+        ctx.fillRect(-1.5 + (shp.x - box.x) * sc, -1.5 + canvas.height - (shp.y - box.y) * sc, 3, 3);
+      }
+      if (style.strokeStyle) {
+        ctx.strokeRect(-1.5 + (shp.x - box.x) * sc, -1.5 + canvas.height - (shp.y - box.y) * sc, 3, 3);
+      }
+    }
+  }
+  
+  if ((style.textFill || style.textStroke) && style.textProp) {
+  
+    if (!style.helper) {
+      style.helper = document.createElement('canvas');
+      style.helper.width = canvas.width;
+      style.helper.height = canvas.height;
+      // TODO: fix for IE?
+    }
+
+    var helper = style.helper.getContext('2d');
+    helper.clearRect(0,0,style.helper.width,style.helper.height);
+    helper.fillStyle = 'black';
+    
+//    var helper = ctx;
+
+    if (style.font) ctx.font = style.font;
+    if (style.textFill) ctx.fillStyle = style.textFill;
+    if (style.textStroke) {
+      ctx.strokeStyle = style.textStroke;
+      ctx.lineJoin = 'round';
+      ctx.lineCap = 'round';
+      ctx.lineWidth = style.textHalo || 1;
+    }
+    ctx.textAlign = 'left';
+    ctx.textBaseline = 'middle';
+    for (var i = 0; i < records.length; i++) {
+      if (data[i] && data[i].values && data[i].values[style.textProp]) {
+        var record = records[i];
+        if (record.shapeType == ShpType.SHAPE_POINT) {
+          var shp = record.shape;
+          var text = trim(data[i].values[style.textProp]);
+          var tx = Math.round(3 + (shp.x - box.x) * sc);
+          var ty = Math.round(canvas.height - (shp.y - box.y) * sc);
+          var tw = Math.round(ctx.measureText(text).width);
+          var th = 12;
+
+          if (tx < 0 || tx+tw > canvas.width || ty < 0 || ty > canvas.height) continue;
+          
+          var img = helper.getImageData(tx+tw/2,ty,1,1);
+          if (img.data[3]) continue;
+          var img = helper.getImageData(tx,ty-th/2,1,1);
+          if (img.data[3]) continue;
+          img = helper.getImageData(tx+tw,ty-th/2,1,1);
+          if (img.data[3]) continue;
+          img = helper.getImageData(tx+tw,ty+th/2,1,1);
+          if (img.data[3]) continue;
+          img = helper.getImageData(tx,ty+th/2,1,1);
+          if (img.data[3]) continue;
+          
+          helper.fillRect(tx, ty-th/2, tw, th);
+          
+          if (style.textStroke) ctx.strokeText(text, tx, ty);
+          if (style.textFill) ctx.fillText(text, tx, ty);
+        }
+      }
+      else {
+        log(data[i].values);
+      }
+    }
+  }
+  
+  t2 = new Date().getTime();
+  log('done rendering in ' + (t2 - t1) + ' ms');
+}
+
+// from http://blog.stevenlevithan.com/archives/faster-trim-javascript
+function trim(str) {
+  var str = str.replace(/^\s\s*/, ''),
+    ws = /\s/,
+    i = str.length;
+  while (ws.test(str.charAt(--i)));
+  return str.slice(0, i + 1);
+}
+
+function renderPolygons(canvas, records, data, box, style) {
+
+  log('rendering polygons');
+
+  var t1 = new Date().getTime();
+  log('starting rendering...');
+
+  var ctx = canvas.getContext('2d');
+  
+  var sc = Math.min(canvas.width / box.width, canvas.height / box.height);
+
+  if (style) {
+    for (var p in style) {
+      ctx[p] = style[p];
+    }
+  }
+  for (var i = 0; i < records.length; i++) {
+    var record = records[i];
+    if (record.shapeType == ShpType.SHAPE_POLYGON || record.shapeType == ShpType.SHAPE_POLYLINE) {
+      var shp = record.shape;
+      ctx.beginPath();
+      for (var j = 0; j < shp.rings.length; j++) {
+        var ring = shp.rings[j];
+        if (ring.length < 1) continue;
+        ctx.moveTo((ring[0].x - box.x) * sc, canvas.height - (ring[0].y - box.y) * sc);
+        for (var k = 1; k < ring.length; k++) {
+          ctx.lineTo((ring[k].x - box.x) * sc, canvas.height - (ring[k].y - box.y) * sc);
+        }
+      }
+      if (style.fillStyle && record.shapeType == ShpType.SHAPE_POLYGON) {
+        ctx.fill();
+      }
+      if (style.strokeStyle) {
+        ctx.stroke();
+      }
+    }
+  }
+  t2 = new Date().getTime();
+  log('done rendering in ' + (t2 - t1) + ' ms');
+}
---
+++ src/map.js
@@ -0,0 +1,125 @@
+function log(s) {
+  if (window.console && window.console.log) console.log(s);
+}
+
+function Map(id, layers) {
+
+  var parent = document.getElementById(id);
+  if (!parent.style.position) parent.style.position = 'relative';
+
+  for (var i = 0; i < layers.length; i++) {
+    log('creating canvas...');
+
+    var layer = layers[i];
+
+    var canvas = document.createElement('canvas');
+    canvas.style.position = 'absolute';
+    canvas.style.left = '0px';
+    canvas.style.top = '0px';
+    // TODO get these from parent properties or in constructor:
+    canvas.style.width = '1024px';
+    canvas.style.height = '512px';
+    parent.appendChild(canvas);
+    if (window.G_vmlCanvasManager) {
+      canvas = G_vmlCanvasManager.initElement(canvas); 
+    }  
+    canvas.width = parseInt(canvas.style.width.match(/\d+/));
+    canvas.height = parseInt(canvas.style.height.match(/\d+/));
+
+    layer.canvas = canvas;
+    layer.box = { x: -180, y: -90, width: 360, height: 180 };
+    
+    layer.load();
+  }
+  
+  var box = { x: -180, y: -90, width: 360, height: 180 };
+  
+  var render = function() {
+    for (var i = 0; i < layers.length; i++) {
+      layers[i].box = box;
+      layers[i].render();
+    }
+  }
+  
+  var panBy = function(x,y) {
+    var degreesPerPixel = box.width / 1024.0;
+    box.x -= x * degreesPerPixel;
+    box.y += y * degreesPerPixel;
+    render();
+  }
+  
+  var zoomBy = function(s,x,y) {
+    var degreesPerPixel = box.width / 1024.0;
+    var boxX = box.x + (x * degreesPerPixel)
+    var boxY = box.y + ((512-y) * degreesPerPixel)
+    box.x -= boxX;
+    box.y -= boxY;
+    box.x *= s;
+    box.y *= s;
+    box.width *= s;
+    box.height *= s;
+    box.x += boxX;
+    box.y += boxY;
+    render();
+  }
+  
+  var mouseDown = function(e) {
+    var prevMouse = { x: e.clientX, y: e.clientY };
+    var mouseMove = function(e) {
+      panBy(e.clientX - prevMouse.x, e.clientY - prevMouse.y);
+      prevMouse.x = e.clientX;
+      prevMouse.y = e.clientY;
+      e.preventDefault(); // hopefully no selecting
+    }
+    var mouseUp = function(e) {
+      document.body.style.cursor = null;
+      document.removeEventListener('mousemove', mouseMove, false);
+      document.removeEventListener('mouseup', mouseUp, false);
+    }
+    document.body.style.cursor = 'hand';
+    document.addEventListener('mousemove', mouseMove, false);
+    document.addEventListener('mouseup', mouseUp, false);
+  };
+  
+  parent.addEventListener('mousedown', mouseDown, false);
+
+  var mouseWheel = function(e) {
+
+    var localX = e.clientX;
+    var localY = e.clientY;
+  
+    // correct for scrolled document
+    localX += document.body.scrollLeft + document.documentElement.scrollLeft;
+    localY += document.body.scrollTop + document.documentElement.scrollTop;
+
+    // correct for nested offsets in DOM
+    for(var node = parent; node; node = node.offsetParent) {
+      localX -= node.offsetLeft;
+      localY -= node.offsetTop;
+    }  
+    
+    var delta = 0;
+    if (e.wheelDelta) {
+        delta = e.wheelDelta;
+    }
+    else if (e.detail) {
+        delta = -e.detail;
+    }
+  
+    if (delta > 0) {
+      zoomBy(0.9, localX, localY);
+    }
+    else if (delta < 0) {
+      zoomBy(1.1, localX, localY);
+    }
+    
+    // cancel page scroll
+    e.preventDefault();
+  }
+
+  // Safari
+  parent.addEventListener('mousewheel', mouseWheel, false);
+  // Firefox
+  parent.addEventListener('DOMMouseScroll', mouseWheel, false);
+  
+}
