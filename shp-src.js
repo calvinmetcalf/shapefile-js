083b50861a4b33fd9df03ae9e662038ade129b8c
Calvin Metcalf <calvin.metcalf@state.ma.us>
2013-06-14 16:24:26 -0400

    baby steps

--- shp-src.js
+++ shp-src.js
@@ -1,4 +1,5 @@
-var binaryAjax = function(url){
+(function(exports){
+shp.binaryAjax = function(url){
 	var promise = shp.deferred();
 	var ajax = new XMLHttpRequest();
 	ajax.onreadystatechange=callback;
@@ -30,12 +31,140 @@ var parseHeader = function(buffer){
 		]
 	}/*)*/;
 }
+function isClockWise(array){
+	var sum = 0;
+	var i = 1;
+	var len = array.length;
+	var prev,cur;
+	while(i<len){
+		prev = cur||array[0];
+		cur = array[i];
+		sum += ((cur[0]-prev[0])*(cur[1]+prev[1]));
+		i++;
+	}
+	return sum > 0;
+}
+function polyReduce(a,b){
+	if(isClockWise(b)){
+		a.push([b]);
+	}else{
+		a[a.length-1].push(b);
+	}
+	return a;
+}
+function parsePoint(data,offset){
+	offset = offset||0;
+	return [data.getFloat64(offset,true),data.getFloat64(offset+8,true)]
+	}
+function parsePointArray(data,offset,num){
+	var out = [];
+	var done = 0;
+	while(done<num){
+		out.push(parsePoint(data,offset));
+		offset += 16;
+		done++;
+	}
+	return out;
+}
+function parseArrayGroup(data,offset,partOffset,num,tot){
+	var out = [];
+	var done = 0;
+	var curNum,nextNum=0,pointNumber;
+	while(done<num){
+		done++
+		partOffset += 4;
+		curNum = nextNum;
+		if(done===num){
+			nextNum = tot;
+		}else{
+			nextNum = data.getInt32(partOffset,true);
+		}
+		pointNumber = nextNum - curNum;
+		if(!pointNumber){
+			continue;
+		}
+		out.push(parsePointArray(data,offset,pointNumber));
+		offset += (pointNumber<<4);
+	}
+	return out;
+}
+function parseMultiPoint(data){
+	var out = {};
+	out.bbox = [
+		data.getFloat64(0,true),
+		data.getFloat64(8,true),
+		data.getFloat64(16,true),
+		data.getFloat64(24,true)
+	];
+	var num = data.getInt32(32,true);
+	var done = 0;
+	var offset = 36;
+	if(num===1){
+		out.type = "Point";
+		out.coordinates = parsePoint(data,offset)
+	}else{
+		out.type = "MultiPoint";
+		out.coordinates = parsePointArray(data,offset,num);
+	}
+	return out;
+}
+function parsePolyline(data){
+	var out = {};
+	out.bbox = [
+		data.getFloat64(0,true),
+		data.getFloat64(8,true),
+		data.getFloat64(16,true),
+		data.getFloat64(24,true)
+	];
+	var numParts = data.getInt32(32,true);
+	var num = data.getInt32(36,true);
+	var offset,partOffset;
+	if(numParts === 1){
+		out.type = "LineString";
+		offset = 44;
+		out.coordinates = parsePointArray(data,offset,num);
+	}else{
+		out.type = "MultiLineString";
+		offset = 40 + (numParts<<2);
+		partOffset = 40;
+		out.coordinates = parseArrayGroup(data,offset,partOffset,numParts,num);
+	}
+	return out;
+}
+function parsePolygon(data){
+	var out = parsePolyline(data);
+	if(out.type === "LineString"){
+		out.type = "Polygon";
+		return out;
+	}else{
+		out.coordinates = out.coordinates.reduce(polyReduce,[]);
+		if(out.coordinates.length === 1){
+			out.type = "Polygon";
+			out.coordinates = out.coordinates[0];
+			return out;
+		}else{
+			out.type = "MultiPolygon";
+			return out;
+		}
+	}
+}
 var shpFuncs = [
 	null,
 	function(data){
-		return [data.getFloat64(0,true),data.getFloat64(8,true)]
-	}
-	]
+		return {
+			"type": "Point",
+			"coordinates":parsePoint(data)
+		}
+	},
+	null,
+	parsePolyline,
+	null,
+	parsePolygon,
+	null,
+	null,
+	parseMultiPoint
+	];
+	
 var getRow = function(buffer,offset){
 	var view = new DataView(buffer,offset,12);
 	var len = view.getInt32(4,false)<<1;
@@ -69,3 +198,7 @@ var parseShp = function(buffer){
 	var headers = parseHeader(buffer);
 	return getRows(buffer,shpFuncs[headers.shpCode]);
 }
+shp.getShp = function(base){
+	return shp.binaryAjax(base+'.shp').then(parseShp);
+}
+})(window);
