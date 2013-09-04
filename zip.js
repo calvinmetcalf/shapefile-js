35609bf49155e184d1e00089d16554b2408b5d15
Calvin Metcalf <calvin.metcalf@state.ma.us>
2013-09-04 08:44:49 -0400

    fix zip

--- zip.js
+++ zip.js
@@ -1,6 +1,6 @@
 requirejs.config({
     //By default load any module IDs from js/lib
-    baseUrl: './src',
+    baseUrl: '.',
     //except, if the module ID starts with "app",
     //load it from the js/app directory. paths
     //config is relative to the baseUrl, and
