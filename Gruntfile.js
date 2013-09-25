d9decb8a05036d9acc65e670c8ea2aa5dd1e0ad7
Calvin Metcalf <calvin.metcalf@state.ma.us>
2013-09-25 14:46:58 -0400

    rewrite

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,40 +1,19 @@
-var estraverse = require('estraverse');
-var esprima = require('esprima');
-var escodegen = require('escodegen');
 module.exports = function(grunt) {
-	function rename(){
-		var ast = esprima.parse(grunt.file.read('./dist/shp.js'));
-		estraverse.traverse(ast,{leave:function(node, parent) {if (node.type == 'Identifier'&&node.name==='require'){node.name = '___forBrowserify___';}}});
-		grunt.file.write('./dist/shp.js',escodegen.generate(ast));
-	}
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-		requirejs:{
-        all:{options:{
-          out: "./dist/shp.js",
-          baseUrl: ".",
-          paths:{
-          	jszip:'node_modules/jszip/jszip',
-          	proj4:'node_modules/proj4js/proj4'
-          },
-          wrap: {
-            startFile: 'almond/top.frag',
-            endFile: 'almond/end.frag'
-          },
-          name: 'node_modules/almond/almond',
-          include: ['shp'],
-          optimize:'uglify2',
-          //optimize:'none',
-          uglify2:{
-           mangle: true
-          },
-          preserveLicenseComments: false
-        }
-      }
-      }
-      });
-	grunt.loadNpmTasks('grunt-contrib-requirejs');
-	grunt.registerTask('test', ['connect','mocha_phantomjs']);
-	grunt.registerTask('rename',rename);
-	grunt.registerTask('default', ['requirejs','rename']);
-}
+		component: {
+			build: {
+				options: {
+					args: {
+						out: 'dist',
+						name: '<%= pkg.name %>',
+						//"no-require":true,
+						standalone: 'shp'
+					}
+				}
+			}
+		}
+	});
+	grunt.loadNpmTasks('grunt-component');
+	grunt.registerTask('default', ['component']);
+};
